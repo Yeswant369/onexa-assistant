ceb90abbf0ea56e99a3eb7d4f12dedef67e6ccc5
Yeswant369 <yeswantsai9@gmail.com>
2026-02-19 11:58:00 +0530

    Update knowledge.py

--- knowledge.py
+++ knowledge.py
@@ -31,16 +31,38 @@ Ideal For:
 - Beginners in coding
 - Students aiming for tech careers
 
-3) Communication & Personality Development
+3) Communication , Personality Development & Up skilling
 Duration: 3 months
 Includes:
 - Public speaking training
 - Interview preparation
 - Confidence building
 - Mock interviews with feedback
+-Time management
+-Critical thinking
+-
 Ideal For:
 - College students
 - Job aspirants
+
+
+3)Hobbies & Holistic Development
+-Music
+-Art
+-Dance
+-Yoga
+-sports coaching
+Ideal For:
+-Any age group.
+-Any one looking to improve flexibility(body and mind)
+-Students
+-Any age group.
+
+4)Professional Design courses
+-Fashion Design
+-Graphic Design
+-UI/UX Design
+-Interior Design
 
 ENROLLMENT PROCESS:
 1. Request consultation call
@@ -48,14 +70,22 @@ ENROLLMENT PROCESS:
 3. Personalized roadmap creation
 4. Program onboarding
 
+5)Customized Business & Startup Solutions
+-Sales Operations
+-Marketing
+-Social Media
+-Content Creation and etc...
+
+
 SUPPORT POLICY:
 - Enrollment issues → Ask for Customer ID
 - Technical problems → Redirect to support
 - Pricing details → Suggest consultation call
 - Unrelated questions → Politely refuse
+-(If you have a specific requirement, let us know — we’ll check with our expert team and confirm)
 
 CONTACT SUPPORT:
 Email: support@onexa.com
 Phone: +91-9000000000
-Consultation booking available on request.
+Book a free Counseling session and our experts will guide to the most suitable program.
 """
